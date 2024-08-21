68382c5b67d904189f4ab4222e9e0b9c4497a2c3
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-08-21 15:48:39 +0530

    application properties updated

---
+++ src/main/java/com/codexnovas/cricketInfoBackend/CricketInfoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codexnovas.cricketInfoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketInfoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketInfoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codexnovas/cricketInfoBackend/CricketInfoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexnovas.cricketInfoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketInfoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
